bd5e7ba25abc4a6b44e7597290b501b21542d044
StudentFPT <82350257+StudentFPT@users.noreply.github.com>
2021-04-14 09:01:56 +0700

    Create invoice.php

---
+++ invoice.php
@@ -0,0 +1,23 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="jquery.mobile-1.4.5/jquery.mobile-1.4.5.min.css" />
+    <script src="jquery.mobile-1.4.5/jquery-1.11.1.min.js"></script>
+    <script src="jquery.mobile-1.4.5/jquery.mobile-1.4.5.min.js"></script>
+</head>
+<body>
+    <form method="post" action="productscript.php">
+    Id: <input type="text" name="id" id="id"><br>
+    Product name: <input type="text" name="productname" id="productname"><br>
+    Category: <input type="text" name="category" id="category"><br>
+    Price: <input type="text" name="price" id="price"><br>
+    Descriptions: <input type="text" name="description" id="description"><br>
+    <input type="submit">
+    </form>
+    
+</body>
+</html>
