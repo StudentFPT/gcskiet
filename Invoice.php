cad8d955cc7c0a06e55ee2213c0ed73343758d41
StudentFPT <82350257+StudentFPT@users.noreply.github.com>
2021-04-14 09:33:49 +0700

    Update Invoice.php

--- Invoice.php
+++ Invoice.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <title>INVOICE</title>
     <link rel="stylesheet" href="./jquery mobile 1.4.5/jquery.mobile-1.4.5.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <script src="./jquery mobile 1.4.5/jquery-1.11.1.min.js"></script>
     <script src="./jquery mobile 1.4.5/jquery.mobile-1.4.5.min.js"></script>
+
 </head>
 <body>
     <h2>INVOICE - ATN COMPANY</h2>
@@ -36,5 +38,9 @@
 
         
     </form>
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
 </body>
 </html>
