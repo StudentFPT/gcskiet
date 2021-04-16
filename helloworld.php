3e0376852f566f8f97d49db7d76ee0d664e40e21
StudentFPT <82350257+StudentFPT@users.noreply.github.com>
2021-04-16 09:19:13 +0700

    Update helloworld.php

--- helloworld.php
+++ helloworld.php
@@ -6,8 +6,29 @@
 <?php
 
     echo "Hello World";
-    
+?>
 
+<?php
+    echo "nav
+            <ul>
+                <li class="menu_item"><a href="#">Home</a></li>
+                <li class="menu_item"><a href="#">WorldPress</a></li>
+                <li class="menu_item""><a href="#">Web Design</a>
+                    <ul>
+                        <li><a href="#">Resources</a>
+                        <li><a href="#">Links</a>
+                        <li><a href="#">Other</a>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+        <li class="menu_item"><a href="#">Graphic design</a></li>
+        <li class="menu_item"><a href="#">Inspiration</a></li>
+        <li class="menu_item"><a href="#">Contact</a></li>
+        <li class="menu_item"><a href="#">About</a></li>
+        </ul>
+        </nav>"
+        echo $html_content;
 ?>
 
 </body>
