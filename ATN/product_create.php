6d95da0684518aff1e94391d0556332829b1de64
StudentFPT <82350257+StudentFPT@users.noreply.github.com>
2021-04-19 09:23:29 +0700

    Update product_create.php

--- ATN/product_create.php
+++ ATN/product_create.php
@@ -21,5 +21,27 @@
         echo $date;
         echo $price;
         echo $desc;
+
+        $host="ec2-3-233-43-103.compute-1.amazonaws.com";
+        $database="daoeb2h1i4gref";
+        $user = "ljguzytsklzppr";
+        $port = "5432";
+        $password = "88ba687e8bbe5cbd0741417086045a25cc5bb0d1b3d35ab35cfea4c48680e1fa";
+
+        $host_param_str = "host=".$host;
+        $dbname_param_str = "dbname=".$database;
+        $port_param_str = "port=".$port;
+        $user_param_str = "user=".$user;
+        $pass_param_str = "pass=".$password;
+        $sslmode_param_str = "ssl require";
+
+        $connection_string = $host_param_str + $dbname_param_str + $port_param_str + $user_param_str + $pass_param_str + $sslmode_param_str;
+        $connetion = pg_connect($connection_string);
+
+        if ($connection === false){
+            die("ERROR: Could not connect to the database");
+        }else{
+            echo "SUCCESS: Connection to Heroku Postgres has been established"
+        }
     ?>
 </html>
