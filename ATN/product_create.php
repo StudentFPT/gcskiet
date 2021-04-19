3fd4d166c8bde8811a3339be99052d669f1a3bb7
StudentFPT <82350257+StudentFPT@users.noreply.github.com>
2021-04-19 09:00:54 +0700

    ATN

--- ATN/product_create.php
+++ ATN/product_create.php
@@ -1,7 +1,25 @@
-<?php
-    echo "This file is named after user are submitting the HTML product form";
+<html>
+    <body>
+        <h1>Sale revenue by Category</h1>
+    </body>
 
+    <?php
+    echo "<p>This file is named after user have submmited the HTML product form<p>";
+    ?>
 
+    <?php
+        $id = $_REQUEST["id"];
+        $name = $_REQUEST["name"];
+        $cat = $_REQUEST["cat"];
+        $date = $_REQUEST["date"]
+	    $price = $_REQUEST['price'];
+	    $desc = $_REQUEST['desc'];
 
-
-?>
+        echo $id;
+        echo $name;
+        echo $cat;
+        echo $date;
+        echo $price;
+        echo $desc;
+    ?>
+</html>
