f7edc3fe7efe51216169e862aa17b3a84d9bea3e
StudentFPT <82350257+StudentFPT@users.noreply.github.com>
2021-04-23 10:49:37 +0700

    Update product_create.php

--- ATN/product_create.php
+++ ATN/product_create.php
@@ -1,26 +1,27 @@
+<!DOCTYPE html>
 <html>
-    <body>
-        <h1>Sale revenue by Category</h1>
-    </body>
+<body>
 
-    <?php
-    echo "<p>This file is named after user have submmited the HTML product form<p>";
-    ?>
+<h1>Sale revenue by Category</h1>
 
-    <?php
-        $id = $_REQUEST["id"];
-        $name = $_REQUEST["name"];
-        $cat = $_REQUEST["cat"];
-        $date = $_REQUEST["date"]
-	    $price = $_REQUEST["price"];
-	    $desc = $_REQUEST["desc"];
-
-        echo $id;
-        echo $name;
-        echo $cat;
-        echo $date;
-        echo $price;
-        echo $desc;
+<?php
+echo "This is income by different product category";
+?>
+<!--result:Hello World! kieu text-->
+<?php
+	$id = $_REQUEST["id"];
+	$name = $_REQUEST["name"];
+	$cat = $_REQUEST["cat"];
+	$date = $_REQUEST["date"];
+	$price = $_REQUEST["price"];
+	$desc = $_REQUEST["desc"];
+	
+	echo $id;
+	echo $name;
+	echo $cat;
+	echo $date;
+	echo $price;
+	echo $desc;
 
         $host="ec2-3-233-43-103.compute-1.amazonaws.com";
         $database="daoeb2h1i4gref";
@@ -29,33 +30,38 @@
         $password = "88ba687e8bbe5cbd0741417086045a25cc5bb0d1b3d35ab35cfea4c48680e1fa";
 
         $host_param_str = "host=".$host;
-        $dbname_param_str = "dbname=".$database;
-        $port_param_str = "port=".$port;
-        $user_param_str = "user=".$user;
-        $pass_param_str = "password=".$password;
-        $sslmode_param_str = "sslmode= require";
+	$dbname_param_str = " dbname=".$database;
+	$port_param_str = " port=".$port;
+	$user_param_str = " user=".$user;
+	$pass_param_str = " password=".$password;
+	$sslmode_param_str = " sslmode=require";
 
-        $connection_string = $host_param_str.$dbname_param_str.$port_param_str.$user_param_str.$pass_param_str.$sslmode_param_str;
-        
-        echo "<p>".$connection_string."</p>";
-        
-        $connetion = pg_connect($connection_string);
-        if ($connection === false){
-            die("ERROR: Could not connect to the database");
-        }else{
-            echo "SUCCESS: Connection to Heroku Postgres has been established";
+	$connection_string = $host_param_str.$dbname_param_str.$port_param_str.$user_param_str.$pass_param_str.$sslmode_param_str;
 
-            $product_query = 'INSERT INTO public."Product"(id,product_name,category,descriptions,price) 
-            VALUES (\''.$id.'\',\''.$name.'\',\''.$cat.'\',\''.$desc.'\','.$price.')';
+	echo "<p>".$connection_string."</p>";
 
-            echo '<p>' .$product_query. '</p>';
+	$connection = pg_connect($connection_string);
 
+	if ($connection === false){
+		die("ERROR: Could not connect to database");
+	} else
+	{
+		echo "SUCCESS: Connection to Heroku Postgres has been established";
 
-		    if (pg_query($connection,$product_query)){
-			    echo '<p>SUCCESS: Record is added succesfully. A new product is created</p>';
-		    }else{
-			    echo '<p>ERROR: Could not execute query</p>';
-		    }
-        }
-    ?>
+		$product_query = 'INSERT INTO public."Product"(id,product_name,category,descriptions,price) VALUES (\''.$id.'\',\''.$name.'\',\''.$cat.'\',\''.$desc.'\','.$price.')';
+
+		echo '<p>'.$product_query.'</p>';
+
+		if (pg_query($connection,$product_query)){
+			echo '<p>SUCCESS: Record is added succesfully. A new product is created</p>';
+		}
+		else{
+			echo '<p>ERROR: Could not execute query</p>';
+		}
+
+	}
+	
+?>
+
+</body>
 </html>
