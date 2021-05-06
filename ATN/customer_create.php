3b9345396885c75be840b67809d929e05a09fdd7
StudentFPT <82350257+StudentFPT@users.noreply.github.com>
2021-05-06 15:07:02 +0700

    Update customer_create.php

--- ATN/customer_create.php
+++ ATN/customer_create.php
@@ -48,7 +48,7 @@ echo "This is Customer";
 		echo "SUCCESS: Connection to Heroku Postgres has been established";
 
 		$customer_query = 'INSERT INTO public."Customer"(Email,Id,"Name",Phone,"Address") VALUES 
-		(\''.$id.'\',\''.$email.'\',\''.$name.'\',\''.$phone.'\',\''.$address.'\')';
+		(\''.$email.'\',\''.$id.'\',\''.$name.'\',\''.$phone.'\',\''.$address.'\')';
 
 		echo '<p>'.$customer_query.'</p>';
 
