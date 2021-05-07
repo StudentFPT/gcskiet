aac3f55aadc6348cd99f6369e528ef5be895f5db
StudentFPT <82350257+StudentFPT@users.noreply.github.com>
2021-05-07 09:01:22 +0700

    Update invoice_create.php

--- ATN/invoice_create.php
+++ ATN/invoice_create.php
@@ -49,8 +49,8 @@
         $pass_param_str = "password=".$password;
         $sslmode_param_str = "sslmode= require";
 
-        $connection_string = $host_param_str.$dbname_param_str.$port_param_str.$user_param_str.$pass_param_str.$sslmode_param_str;
-        //$connection_string = "host=".$host." dbname=".$database." user=".$user." port=".$port." password=".$password." sslmode=require";
+        //$connection_string = $host_param_str.$dbname_param_str.$port_param_str.$user_param_str.$pass_param_str.$sslmode_param_str;
+        $connection_string = "host=".$host." dbname=".$database." user=".$user." port=".$port." password=".$password." sslmode=require";
 
         echo "<p>".$connection_string."</p>";
         
@@ -61,7 +61,7 @@
         }else{
             echo "SUCCESS: Connection to Heroku Postgres has been established";
 
-            $invoice_query = 'INSERT INTO public."invoice"("customer_id" , "customer_name" , "invoice_id" , "invoice_date" , "invoice_product_id" , "invoice_product_quantity") VALUES 
+            $invoice_query = 'INSERT INTO public."invoice"("customer_id" , "customer_name" , "invoice_id" , "invoice_date" , "invoice_product_id" ,"invoice_product_quantity") VALUES 
             (\''.$cusid.'\',\''.$cusname.'\',\''.$invoiceid.'\',\''.$invoicedate.'\',\''.$invoice_product_id.'\',\''.$invoice_product_quantity.'\')';
 
             echo '<p>' .$invoice_query. '</p>';
